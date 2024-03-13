690231ed7bd24e3c7c49f399644cc6bf8069c8ed
GDK <igdkaur@gmail.com>
2024-03-13 17:25:06 +0530

    shuffle answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -17,12 +17,15 @@ export default function Quiz() {
     })
   }
 
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
