107e5b62422b5ea564fee926185349bb650ac5bd
GDK <igdkaur@gmail.com>
2024-03-13 16:16:05 +0530

    output question wip

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,14 +3,34 @@
 // change to a different question whenever the user answers a question
 // store those answers
 
-import { useState } from "react"
-import QUESTIONS from '../questions'
+import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  return <p>current question</p>
+  function handleSelect(selectedAnswer) {
+
+    setUserAnswers((prevstate) => {
+      return [...prevstate, selectedAnswer]
+    })
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-
