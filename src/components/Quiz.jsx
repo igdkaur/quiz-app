e3c5b533ffc3e34f9a969229be343657db177950
GDK <igdkaur@gmail.com>
2024-03-13 17:43:12 +0530

    quiz complete handle

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,21 +5,31 @@
 
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
-
     setUserAnswers((prevstate) => {
-      return [...prevstate, selectedAnswer]
-    })
+      return [...prevstate, selectedAnswer];
+    });
   }
 
+    if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+  
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
-
+  
   return (
     <div id="quiz">
       <div id="question">
